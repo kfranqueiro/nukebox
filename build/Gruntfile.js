f1afe15cdf49b042032e1e4ff8dc635b32e98184
Kenneth G. Franqueiro <kenneth.franqueiro@gmail.com>
2015-03-29 19:48:33 -0400

    Modify Gruntfile so grunt-contrib-watch actually works

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -6,20 +6,26 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-stylus');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.file.setBase(require('path').resolve('..'));
+	// Note: Paths below generally all start with .. since this file is within the build subfolder.
+	// Normally, atom-shell apps promote calling grunt.file.setBase to avoid this, but...
+	// https://github.com/gruntjs/grunt-contrib-watch/issues/426
 
 	grunt.initConfig({
 		'build-atom-shell': {
 			tag: 'v0.22.2',
 			nodeVersion: '0.22.0',
 			buildDir: os.tmpdir() + '/atom-shell',
+			targetDir: '../atom-shell',
 			projectName: 'nukebox',
 			productName: 'Nukebox'
 		},
 
 		clean: {
+			options: {
+				force: true // Unfortunately necessary due to this being under the build subfolder
+			},
 			css: {
-				src: [ 'src/resources/**/*.css' ]
+				src: [ '../src/resources/**/*.css' ]
 			}
 		},
 
@@ -31,13 +37,13 @@ module.exports = function (grunt) {
 			},
 
 			compile: {
-				files: { 'src/resources/nukebox.css': 'src/resources/nukebox.styl' }
+				files: { '../src/resources/nukebox.css': '../src/resources/nukebox.styl' }
 			}
 		},
 
 		watch: {
 			stylus: {
-				files: [ 'src/resources/**/*.styl' ],
+				files: [ '../src/resources/**/*.styl' ],
 				tasks: [ 'stylus' ]
 			}
 		}
