a73fd6df115b827bfa8df40f3529a8159ccdb3cf
Kenneth G. Franqueiro <kenneth.franqueiro@gmail.com>
2015-04-25 17:10:23 -0400

    Split out rename-electron task and replace version number

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -1,47 +1,13 @@
-var fs = require('fs');
-var path = require('path');
-
-function rename(basePath, oldName, newName) {
-	fs.renameSync(path.resolve(basePath, oldName), path.join(basePath, newName));
-}
-
 module.exports = function (grunt) {
+	var electronVersion = '0.25.1';
+	var appVersion = grunt.file.readJSON('../package.json').version;
+
 	grunt.loadNpmTasks('grunt-download-electron');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-stylus');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-text-replace');
-
-	grunt.registerTask('rename-electron', 'Renames the electron app/binary.', function () {
-		var originalName = 'Electron';
-		var lcOriginalName = originalName.toLowerCase();
-		var name = grunt.config.get('rename-electron').name;
-		var lcName = name.toLowerCase();
-		var outputDir = grunt.config.get('download-electron').outputDir;
-
-		// Mac OS X (rename ALL the things!)
-		grunt.file.expand({
-			cwd: '../electron'
-		}, [
-			'**/MacOS/Electron Helper*',
-			'**/MacOS/Electron',
-			'**/Electron Helper*.app',
-			'**/Electron.app'
-		]).forEach(function (filename) {
-			var destination = path.join(path.dirname(filename),
-				path.basename(filename).replace(originalName, name));
-			rename('../electron', filename, destination);
-		});
-
-		if (grunt.file.exists(outputDir, lcOriginalName + '.exe')) {
-			// Windows
-			rename(outputDir, lcOriginalName + '.exe', lcName + '.exe');
-		}
-		else if (grunt.file.exists(outputDir, lcOriginalName)) {
-			// Linux
-			rename(outputDir, lcOriginalName, lcName);
-		}
-	});
+	grunt.loadTasks('tasks');
 
 	// Note: Paths below generally all start with .. since this file is within the build subfolder.
 	// Normally, Electron apps promote calling grunt.file.setBase to avoid this, but...
@@ -49,7 +15,7 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 		'download-electron': {
-			version: '0.25.1',
+			version: electronVersion,
 			outputDir: '../electron'
 		},
 
@@ -67,6 +33,9 @@ module.exports = function (grunt) {
 				replacements: [ {
 					from: 'Electron',
 					to: 'Nukebox'
+				}, {
+					from: electronVersion,
+					to: appVersion
 				} ]
 			}
 		},
