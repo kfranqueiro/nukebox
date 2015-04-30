aad4b7ee60ac1e91e017814d4ec5e1f096383de6
Kenneth G. Franqueiro <kenneth.franqueiro@gmail.com>
2015-04-29 20:38:45 -0400

    Rename View menu to Debug and hide behind an environment variable

--- src/browser/menu.js
+++ src/browser/menu.js
@@ -18,25 +18,6 @@ menu.push(
 		]
 	},
 	{
-		label: '&View',
-		submenu: [
-			{
-				label: '&Reload',
-				accelerator: 'CmdOrCtrl+R',
-				click: function () {
-					BrowserWindow.getFocusedWindow().reloadIgnoringCache();
-				}
-			},
-			{
-				label: 'Toggle &Developer Tools',
-				accelerator: isDarwin ? 'Alt+Cmd+I' : 'Ctrl+Shift+I',
-				click: function () {
-					BrowserWindow.getFocusedWindow().toggleDevTools();
-				}
-			}
-		]
-	},
-	{
 		label: '&Options',
 		submenu: [
 			{
@@ -51,3 +32,27 @@ menu.push(
 		]
 	}
 );
+
+if (process.env.NUKEBOX_DEBUG) {
+	menu.push(
+		{
+			label: '&Debug',
+			submenu: [
+				{
+					label: '&Reload',
+					accelerator: 'CmdOrCtrl+R',
+					click: function () {
+						BrowserWindow.getFocusedWindow().reloadIgnoringCache();
+					}
+				},
+				{
+					label: 'Toggle &Developer Tools',
+					accelerator: isDarwin ? 'Alt+Cmd+I' : 'Ctrl+Shift+I',
+					click: function () {
+						BrowserWindow.getFocusedWindow().toggleDevTools();
+					}
+				}
+			]
+		}
+	);
+}
